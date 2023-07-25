da3a7378f728387a7c9cb0af8cceaa4ba73c07c2
Jason Sprinkle <jason.sprinkle@literati.com>
2023-07-25 11:40:22 -0500

    Add melange theme for lualine

--- lua/sprinkle/packer.lua
+++ lua/sprinkle/packer.lua
@@ -92,7 +92,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true },
         config = function()
-            require("lualine").setup()
+            require("lualine").setup({ options = { theme = 'melange' } })
         end
     }
 

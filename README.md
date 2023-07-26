75a3ab20c06b26d72674210db5d6dbe8ba73407e
Jason Sprinkle <jason.sprinkle91@gmail.com>
2023-07-25 21:45:27 -0500

    ensure packer it set up when running the config

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sprinkle-nvim
+## Setup
+
+If this is your first time doing this on a machine, you have to have packer installed first:
+
+Follow these steps: https://github.com/wbthomason/packer.nvim#bootstrapping
+
+Or just do this
+
+```bash
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+```
--- lua/sprinkle/packer.lua
+++ lua/sprinkle/packer.lua
@@ -1,5 +1,15 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -56,20 +66,20 @@ return require('packer').startup(function(use)
         branch = 'v2.x',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },             -- Required
-            { 'williamboman/mason.nvim' },           -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            { 'williamboman/mason.nvim' }, -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },         -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-            { 'hrsh7th/cmp-buffer' },       -- Optional
-            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Optional
+            { 'hrsh7th/cmp-path' }, -- Optional
             { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },             -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
             { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
@@ -100,7 +110,7 @@ return require('packer').startup(function(use)
     use {
         'tpope/vim-surround',
         config = function()
-            vim.g.surround_115 = "**\r**"  -- 115 is the ASCII code for 's'
+            vim.g.surround_115 = "**\r**" -- 115 is the ASCII code for 's'
             vim.g.surround_47 = "/* \r */" -- 47 is /
         end
     }
@@ -156,4 +166,10 @@ return require('packer').startup(function(use)
             require("leap").add_default_mappings()
         end
     }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)

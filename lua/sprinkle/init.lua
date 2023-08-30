ad5f8a0127687de9e039092507241687cf25b33a
Jason Sprinkle <jason.sprinkle@literati.com>
2023-08-30 10:47:55 -0500

    consolidate all of the sprinkle files into the init lua

--- lua/sprinkle/init.lua
+++ lua/sprinkle/init.lua
@@ -1,3 +1,69 @@
-require("sprinkle.remap")
-require("sprinkle.set")
-require("sprinkle.lazy")
+-- SO LAZY
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    -- bootstrap lazy.nvim
+    -- stylua: ignore
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+        lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+-- I guess lazy vim needs this?
+vim.g.mapleader = " "
+
+require('lazy').setup('plugins')
+
+-- mr worldwide
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.hidden = true
+
+vim.g.python3_host_prog = '/Users/jasonsprinkle/.pyenv/versions/py3nvim/bin/python'
+
+vim.opt.foldmethod = "indent"
+vim.opt.foldenable = false
+
+-- some keymaps
+vim.keymap.set("n", "[w", "<C-w>W")
+vim.keymap.set("n", "]w", "<C-w>w")
+
+-- if we're in a django project, always set these html files to be htmldjango
+vim.api.nvim_create_autocmd({
+    "BufNewFile",
+    "BufRead"
+}, {
+    pattern = { "*.html" },
+    callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+        -- if this is an html file in a django project, then we should
+        -- automatically set the filetype to such
+        if vim.api.nvim_buf_get_name(buf):match("djangoproject") then
+            vim.api.nvim_buf_set_option(buf, "filetype", "htmldjango")
+        end
+    end
+})
--- lua/sprinkle/remap.lua
+++
@@ -1,3 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "[w", "<C-w>W")
-vim.keymap.set("n", "]w", "<C-w>w")

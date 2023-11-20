10a6d206680adf75d9f51a161b693649f066b822
Jason Sprinkle <jason.sprinkle@literati.com>
2023-11-20 09:21:23 -0600

    dapui setup

--- init.lua
+++ init.lua
@@ -218,8 +218,9 @@ require('lazy').setup({
     {
         "mfussenegger/nvim-dap",
         dependencies = {
-            "mfussenegger/nvim-dap-python",
             "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text",
+            "mfussenegger/nvim-dap-python",
             { "mxsdev/nvim-dap-vscode-js", tag = "v1.1.0" },
             {
                 "microsoft/vscode-js-debug",
@@ -600,22 +601,23 @@ vim.keymap.set('n', '<Leader>tb', function() dap.toggle_breakpoint() end)
 vim.keymap.set('n', '<Leader>sb', function() dap.set_breakpoint() end)
 vim.keymap.set('n', '<Leader>lp',
     function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
-vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
-vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-    require('dap.ui.widgets').hover()
-end)
-vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-    require('dap.ui.widgets').preview()
-end)
-vim.keymap.set('n', '<Leader>df', function()
-    local widgets = require('dap.ui.widgets')
-    widgets.centered_float(widgets.frames)
-end)
-vim.keymap.set('n', '<Leader>ds', function()
-    local widgets = require('dap.ui.widgets')
-    widgets.centered_float(widgets.scopes)
-end)
+
+-- dap virtual text
+local dapVirtualText = require("nvim-dap-virtual-text")
+dapVirtualText.setup()
+--
+-- dap ui
+local dapui = require("dapui")
+dapui.setup()
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
 
 -- alpha
 local alpha = require("alpha")
